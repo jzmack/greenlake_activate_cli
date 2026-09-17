7633cbeecdedc9fee408805e003ef22a4883847e
Jake Mackin <canesfanjake1@gmail.com>
2026-09-17 13:15:53 -0400

    added login function and tested good

---
+++ activate_login.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def load_credentials():
+    """Load credentials from .env file"""
+    load_dotenv()
+    return os.getenv("CREDENTIAL_1")
+
+def create_activate_session(credential_1:str):
+    """Login and save session cookie"""
+    login_url = "https://activate.arubanetworks.com/LOGIN"
+    login_data = {
+        'credential_0': "username",
+        'credential_1': credential_1
+    }
+    print(login_data)
+    session = requests.session()
+    response = session.post(login_url, data=login_data)
+    print(f"Login Status Code: {response.status_code}")
+    print(f"Full response:\n{response.text}")
+    return session
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from greenlake-activate-cli!")
+from activate_login import create_activate_session, load_credentials
 
+def main():
+    credentials = load_credentials()
+    create_activate_session(credentials)
 
 if __name__ == "__main__":
     main()
